54c442b71782c4af48b202b5cfbf79fde9c9dc20
Shivam Nagapure <shivamnagapure2005@gmail.com>
2025-03-18 20:01:51 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,7 +33,7 @@ export const register = async (req , res) => {
         res.cookie('token' , token , {
             httpOnly : true ,
             secure : process.env.NODE_ENV === 'production',
-            samesite : process.env.NODE_ENV === 'production' ? 
+            sameSite : process.env.NODE_ENV === 'production' ? 
             'none' : 'strict',
             maxAge : 7 * 24 * 60 * 60 * 1000 
         });
@@ -80,7 +80,7 @@ export const login = async (req , res) => {
         res.cookie('token' , token , {
             httpOnly : true ,
             secure : process.env.NODE_ENV === 'production',
-            samesite :  process.env.NODE_ENV === "production" ? "None" : "Lax" ,
+            sameSite :  process.env.NODE_ENV === "production" ? "None" : "Lax" ,
             maxAge : 7 * 24 * 60 * 60 * 1000 
         });
 
