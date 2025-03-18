f28f2869fbdfe45e052af15684099cdacebf5105
Shivam Nagapure <shivamnagapure2005@gmail.com>
2025-03-18 18:54:04 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
       axios.defaults.withCredentials = true ;
       if(state === 'Sign Up'){
-       const { data } =  await axios.post(backendUrl + '/api/auth/register' , { name , email , password } , { withCredentials: true }) ;
+       const { data } =  await axios.post(backendUrl + '/api/auth/register' , { name , email , password } ) ;
 
        
        
@@ -36,7 +36,7 @@ const Login = () => {
           toast.error(data.message)
         }        
       } else {
-        const { data } =  await axios.post(backendUrl + '/api/auth/login' , { email , password } ,{ withCredentials: true } ) ;
+        const { data } =  await axios.post(backendUrl + '/api/auth/login' , { email , password } ) ;
         
         if(data.success){
           setIsLoggedIn(true)
